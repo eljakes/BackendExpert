781b4df61d3229bd8b222497ce72b550fc2e94cd
Elvis Owusu-Sekyere <elvlon@yahoo.com>
2026-05-24 12:12:40 +0000

    API

--- backend-learning/index.php
+++ backend-learning/index.php
@@ -1,8 +1,3 @@
 <?php
 
-echo "My backend server is running successfully." . "<br>";
-echo "Welcome to my backend engineering journey!" . "<br>";
-echo "Learning PHP and PostgreSQL." . "<br>";
-echo "Excited to build amazing applications!" . "<br>";
-echo "Stay tuned for more updates on my progress." . "<br>";
-echo "Thank you for visiting my backend server!" . "<br>";
+echo "Welcome to My Backend Api.";
