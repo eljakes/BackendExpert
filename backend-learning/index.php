a2359b5ddfdd61cd80c688e8ccbe323ca77229fd
Elvis Owusu-Sekyere <elvlon@yahoo.com>
2026-05-24 11:46:25 +0000

    update on journey

--- backend-learning/index.php
+++ backend-learning/index.php
@@ -1,3 +1,8 @@
 <?php
 
-echo "My backend server is running successfully.";
+echo "My backend server is running successfully." . "<br>";
+echo "Welcome to my backend engineering journey!" . "<br>";
+echo "Learning PHP and PostgreSQL." . "<br>";
+echo "Excited to build amazing applications!" . "<br>";
+echo "Stay tuned for more updates on my progress." . "<br>";
+echo "Thank you for visiting my backend server!" . "<br>";
