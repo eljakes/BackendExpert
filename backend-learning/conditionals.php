800f3a81240406398d093e7b59a06220b1a1474c
Elvis Owusu-Sekyere <elvlon@yahoo.com>
2026-05-24 20:18:57 +0000

    Role conditionals

---
+++ backend-learning/conditionals.php
@@ -0,0 +1,11 @@
+<?php
+
+$role = "Admin";
+
+if ($role == "Admin") {
+    echo "Welcome Admin!";
+} elseif ($role == "Artisan") {
+    echo "Welcome Artisan!";
+} else {
+    echo "Welcome User!";
+}
--- backend-learning/index.php
+++ backend-learning/index.php
@@ -1,3 +1,10 @@
 <?php
 
-echo "Welcome to My Backend Api.";
+$wallet_Balance = 1000.00;
+$payment_Amount = 250.00;
+$isVerified = true;
+
+echo "sufficient balance: " . ($payment_Amount <= $wallet_Balance) . "<br>";
+var_dump($isVerified); 
+echo "<br>";
+echo "Payment Approved: " . ($wallet_Balance >= 1000.00 && $payment_Amount <= $wallet_Balance && $isVerified) . "<br>";
